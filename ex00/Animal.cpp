1388e593a9ae48ee7349ccbfb68428bcd0c30d6e
thee-falcon <omar.makran.10@gmail.com>
2024-02-21 19:18:19 +0100

    working in AnimalCpp

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -3,10 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   Animal.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:12:21 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/19 17:12:22 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/21 19:15:41 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Animal.hpp"
+
+Animal::Animal(std::string type) : protectedType(type) {
+    std::cout << "Constructor:" << type << "is called!" << std::endl;
+}
--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -3,10 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   Animal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:12:27 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/19 17:12:28 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/21 18:30:06 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+class Animal
+{
+protected:
+    std::string protectedType;
+    
+public:
+    Animal(std::string type);
+    ~Animal();
+    
+    void    makeSound();
+};
+
+#endif
