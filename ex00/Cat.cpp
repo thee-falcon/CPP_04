acd8448a3edaf9c2aa8aded7ff400df1cf10f19b
thee-falcon <omar.makran.10@gmail.com>
2024-02-22 17:34:08 +0100

    CatHpp and CatCPP is done

--- ex00/Cat.cpp
+++ ex00/Cat.cpp
@@ -3,10 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   Cat.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:12:33 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/19 17:12:34 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/22 17:19:52 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Cat.hpp"
+
+Cat::Cat( void ) {
+    std::cout << "Default Constructor: " << this->type << " is called!" << std::endl;
+}
+
+Cat::~Cat ( void ) {
+    std::cout << "Default Constructor: " << this->type << " is called!" << std::endl;
+}
+
+Cat::Cat( const Cat& other) {
+    std::cout << "Copy Constructor: " << this->type << " is called!" << std::endl;
+    *this = other;
+}
+
+Cat& Cat::operator=( const Cat& other) {
+    std::cout << "Assignement Operator: " << this->type << " is called!" << std::endl;
+    if (this != &other) {
+        this->type = other.type;
+    }
+    return (*this);
+}
+
+void    Cat::makeSound( void ) const {
+    std::cout << "Meeeeeooooooooowwww!" << std::endl;
+}
--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -3,17 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   Cat.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:12:38 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/21 21:44:12 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/22 17:15:03 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CAT_HPP
+# define CAT_HPP
+
 # include "Animal.hpp"
 
-// class   Cat : public Animal {
+class Cat : public Animal
+{
 
-//     Cat( void );
-//     ~Cat( void );
-// }
+public:
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor:
+    Cat( void );
+    // destructor:
+    ~Cat( void );
+    // copy constructor:
+    Cat( const Cat& other);
+    // assignement operator:
+   Cat& operator=( const Cat& other );
+
+   void   makeSound( void ) const;
+};
+
+
+#endif
